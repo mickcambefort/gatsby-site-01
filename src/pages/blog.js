e6c5452b938ac88b607c84cb79d3aa86c6f00551
mickcambefort <mick.cambefort@gmail.com>
2023-10-13 18:45:01 +0200

    finished gatsby part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,28 +4,31 @@ import Layout from "../components/Layout"
 import Seo from "../components/seo"
 
 const BlogPage = ({ data }) => {
-    const nodes = data.allFile.edges.map(edge => edge.node);
-
     return (
         <Layout pageTitle="My Blog Page">
-            <ul>
-                {nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
-                ))}
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>Title: {node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
 query {
-  allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
-    edges {
-      node {
-        name
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
       }
+      id
+      excerpt
     }
   }
 }
